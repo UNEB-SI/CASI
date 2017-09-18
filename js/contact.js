8df35e7cfed9f40462d94c563eeca9b3088c2cf3
Mateus Nascimento <mateus.nnascimento.s@gmail.com>
2017-09-18 14:42:15 -0300

    Corrigido erros em formulário

--- js/contact.js
+++ js/contact.js
@@ -1,4 +1,20 @@
 function sendForm() {
+	if ($("#nome").val() == "" || 
+		$("#matricula").val() == "" || 
+		$("#email").val() == "" || 
+		$("#assunto").val() == "" || 
+		$("#descricao").val() == "" ) 
+	{
+		swal("Mensagem não enviada", "Você deixou algum campo vazio", "warning");
+		return;
+	}
+
+	if (document.getElementById("email").checkValidity() == false) {
+		swal("Mensagem não enviada", "O email é inválido", "warning");
+		return;
+	}
+
+	document.getElementById('btn_enviar').disabled = true;
 	emailjs.init("user_30W9GaH9rPRlS5fXr3J23");
 	emailjs.send("gmail", "template_kjjsnkdnsssdv", 
 		{ 	sender_name: $("#nome").val(), 
@@ -7,13 +23,13 @@ function sendForm() {
 			sender_subject: $("#assunto").val(),
 			sender_message: $("#descricao").val()
 	}).then(function(response) {
-	   	var notification = document.querySelector('.mdl-js-snackbar');
-		notification.MaterialSnackbar.showSnackbar({message: 'Enviado com sucesso'});
+	   	swal("Mensagem enviada", "Entraremos em contato o mais breve possível ;)", "success");
 		var frm = document.getElementsByName('form')[0];
 		frm.reset();
+		document.getElementById('btn_enviar').disabled = false;
 	}, function(err) {
-	   	var notification = document.querySelector('.mdl-js-snackbar');
-		notification.MaterialSnackbar.showSnackbar({message: 'Erro ao enviar'});
+		swal("Mensagem não enviada", "Entre em contato diretamente pelo nosso email: casi.uneb@gmail.com", "error");
+		document.getElementById('btn_enviar').disabled = true;
 	});
 }
 
